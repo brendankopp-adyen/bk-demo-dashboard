8e8a09ee7f7f7bb2fdd19848f3a4a02a2ab1dc47
brendankopp-adyen <34170813+brendankopp-adyen@users.noreply.github.com>
2020-01-29 15:14:42 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const { ParseServer } = require('parse-server');
 
-const api = new ParseServer({
-  databaseURI: 'mongodb://localhost:27017/dashboard',
-  appId: 'hello',
-  masterKey: 'world',
-  serverURL: 'http://localhost:1338/parse',
-});
-
 const app = express();
+
+//API
+const api = new ParseServer({
+  databaseURI: 'mongodb://bk-demo-app-db-user:dhg73-9SY18$923Xsy%dk23!@ds039125.mlab.com:39125/heroku_39dptd5q',
+  appId: 'bk-demo-app',
+  masterKey: 'bk-demo-app-dummy-master-key-xxx',
+  serverURL: 'https://bk-demo-app.herokuapp.com/parse',
+});
 app.use('/parse', api);
 
-const port = 1338;
+// Serve static assets from the /public folder
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
+// Parse Server plays nicely with the rest of your web routes
+app.get('/', function(req, res) {
+  res.status(200).send('I dream of being a website ... build me!');
+});
+
+const port = 1337;
 const httpServer = require('http').createServer(app);
 
 httpServer.listen(port, () => {
