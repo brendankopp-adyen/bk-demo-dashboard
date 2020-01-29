36f7a5afb9f6ce8d250e4247afffd1820640cc94
brendankopp-adyen <34170813+brendankopp-adyen@users.noreply.github.com>
2020-01-29 15:25:00 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { ParseServer } = require('parse-server');
+const ParseDashboard = require('parse-dashboard');
 
-const app = express();
+//DEFINE THE APP
+var app = express();
 
-//API
-const api = new ParseServer({
+//PARSE SERVER
+var api = new ParseServer({
   databaseURI: 'mongodb://bk-demo-app-db-user:dhg73-9SY18$923Xsy%dk23!@ds039125.mlab.com:39125/heroku_39dptd5q',
   appId: 'bk-demo-app',
   masterKey: 'bk-demo-app-dummy-master-key-xxx',
@@ -12,13 +14,28 @@ const api = new ParseServer({
 });
 app.use('/parse', api);
 
+//PARSE DASHBOARD
+var options = { allowInsecureHTTP: false };
+
+var dashboard = new ParseDashboard({
+  databaseURI: 'mongodb://bk-demo-app-db-user:dhg73-9SY18$923Xsy%dk23!@ds039125.mlab.com:39125/heroku_39dptd5q',
+  appId: 'bk-demo-app',
+  masterKey: 'bk-demo-app-dummy-master-key-xxx',
+  serverURL: 'https://bk-demo-app.herokuapp.com/parse',
+}, options);
+app.use('/dashboard', dashboard);
+
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
+
+//ROUTES
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
   res.status(200).send('I dream of being a website ... build me!');
 });
+
+//START IT
 
 const port = 1337;
 const httpServer = require('http').createServer(app);
@@ -26,3 +43,5 @@ const httpServer = require('http').createServer(app);
 httpServer.listen(port, () => {
   console.log(`parse-server running on port: ${port}`);
 });
+
+
