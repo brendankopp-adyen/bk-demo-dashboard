5e541344efe7eafb421ed66888ed6e8cf40d41d0
brendankopp-adyen <34170813+brendankopp-adyen@users.noreply.github.com>
2020-01-30 14:12:59 -0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var dashboard = new ParseDashboard({
   "apps": [
     {
       "serverURL": "https://bk-demo-app.herokuapp.com/parse",
-      "graphQLServerURL": "http://localhost:1337/graphql",
+      "graphQLServerURL": "https://bk-demo-app.herokuapp.com/graphql",
       "appId": "bk-demo-app",
       "masterKey": "bk-demo-app-dummy-master-key",
       "appName": "BK Demo App",
